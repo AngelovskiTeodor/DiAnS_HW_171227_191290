ef804b0c9743a62c90ac93bc925c5e1e03cb1f9f
viktormiladinov <viktor.miladinov25@yahoo.com>
2021-12-06 23:25:01 +0100

    Update

---
+++ Domasno2/Prototype/web_client/monuments-sk/src/components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "../Header/Header";
+import SearchAppBar from "../SearchAppBar/SearchAppBar";
+import Map from "../Map/Map";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchAppBar />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
