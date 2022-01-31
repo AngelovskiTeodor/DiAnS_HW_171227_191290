ffa2f8a04701f6ff10c473e1cae3ebb72b580430
Teodor Angelovski <angelovski.teo@outlook.com>
2022-01-31 19:14:27 +0100

    ReactApp. Changed api endpoints to AWS provided URIs.

--- Domasno3i4/web_client/monuments-sk/src/api-endpoints.js
+++ Domasno3i4/web_client/monuments-sk/src/api-endpoints.js
@@ -1,4 +1,4 @@
-const MonumentsServiceEndpoint = "https://localhost:9091/";
-const RouteFinderServiceEndpoint = "https://localhost:9092/";
+const MonumentsServiceEndpoint = "http://monuments-env-dians-hw.us-east-1.elasticbeanstalk.com/api/monuments";
+const RouteFinderServiceEndpoint = "http://navigatorengine-env.eba-pkw3ikat.us-east-1.elasticbeanstalk.com/api/route";
 
 export { MonumentsServiceEndpoint, RouteFinderServiceEndpoint };
